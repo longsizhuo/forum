6ec04e6037eaf957b2e368be4669ca7b372227ec
Loong Loong <longsizhuo@gmail.com>
2024-08-24 02:47:28 +1000

    Initial GRPC

--- service/forumService.go
+++ service/forumService.go
@@ -1,7 +1,26 @@
 package service
 
-import pb "github.com/longsizhuo/forum/proto/github.com/longsizhuo/forum/proto/forum_grpc.pb.go"
+import (
+	"context"
+	"github.com/longsizhuo/forum/models"
+	pb "github.com/longsizhuo/forum/proto"
+	"gorm.io/gorm"
+)
 
 type server struct {
 	pb.UnimplementedForumServiceServer
+	db *gorm.DB
+}
+
+func (s *server) CreateUser(ctx context.Context, req *pb.CreateUserRequest) (*pb.CreateUserResponse, error) {
+	user := models.User{
+		UserName:     req.UserName,
+		UserPassword: req.UserPassword,
+		UserSex:      req.UserSex,
+		UserAge:      int(req.UserAge),
+	}
+	if err := s.db.Create(&user).Error; err != nil {
+		return nil, err
+	}
+	return &pb.CreateUserResponse{UserId: int32(user.UID)}, nil
 }
